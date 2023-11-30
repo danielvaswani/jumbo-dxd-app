69706a3a59f2a38032fe304c47cca221c3b73e09
damiancaelen <475794@student.fontys.nl>
2023-11-30 15:57:55 +0100

    Adjusted RecipePage for futureproof dynamic text.

--- jumbo-dxd-web/src/App.jsx
+++ jumbo-dxd-web/src/App.jsx
@@ -1,27 +1,63 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import './RecipePage'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLeaf } from '@fortawesome/free-solid-svg-icons';
+import PastaPesto from './img/recipePic.png';
+
 import RecipePage from './RecipePage'
 
 function App() {
-  const [count, setCount] = useState(0)
-  let [recipes, setRecipes] = useState([])
+  // const [count, setCount] = useState(0)
+  // let [recipes, setRecipes] = useState([])
 
-  useEffect(() => {
-    //fetch recipes from recipes-oct-2.json
-      const data = fetch('./recipes-oct-2.json')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setRecipes([recipes, data])
-      })
-    }, [])
+  // useEffect(() => {
+  //   //fetch recipes from recipes-oct-2.json
+  //     const data = fetch('./recipes-oct-2.json')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log(data)
+  //       setRecipes([recipes, data])
+  //     })
+  //   }, [])
+
+  const recipes = [
+  {
+    ingredients: [
+      "400 g of your favourite spaghetti",
+      <span className="green-leaf"><FontAwesomeIcon icon={faLeaf} /> 1 cup of fresh basil leaves</span>,
+      <span className="green-leaf"><FontAwesomeIcon icon={faLeaf} /> 4 garlic cloves</span>,
+      "1/4 cup olive oil",
+      "Salt and pepper to taste"
+    ],
+    instructions: [
+      "Cook 400 g pasta according to the instructions on the package.",
+      "While the pasta is cooking, chop about 6-8 tomatoes, use 1 cup fresh basil leaves and mince 4 garlic cloves.",
+      "In a pan, heat 1/4 cup of olive oil over medium heat, then add the chopped garlic and fry for a minute.",
+      "Add the chopped tomatoes and cook for a few minutes until soft.",
+      "Stir in the fresh basil and season with salt and pepper.",
+      "Mix the cooked pasta with the tomato basil sauce.",
+      "Serve and, if desired, sprinkle with grated Parmesan cheese."
+    ],
+    title: "Spaghetti with Tomato Basil Sauce",
+    image: PastaPesto,
+    servings: 4,
+    dishType: "Main Dish",
+    cookingTime: 30,
+  }
+  ]
 
   return (
     <>
-    <RecipePage/>
-
-      {recipes[0]}
+    <RecipePage 
+    ingredients={recipes[0].ingredients} 
+    instructions={recipes[0].instructions} 
+    title={recipes[0].title} 
+    image={recipes[0].image} 
+    servings={recipes[0].servings} 
+    dishType={recipes[0].dishType} 
+    cookingTime={recipes[0].cookingTime}
+    />
     </>
   )
 }
--- jumbo-dxd-web/src/RecipePage.jsx
+++ jumbo-dxd-web/src/RecipePage.jsx
@@ -1,68 +1,51 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock, faUtensils, faUsers, faHeart as faHeartRegular, faLeaf } from '@fortawesome/free-solid-svg-icons';
+import { faClock, faUtensils, faUsers, faHeart as faHeartRegular } from '@fortawesome/free-solid-svg-icons';
 import './RecipePage.css';
-import PastaPesto from './img/recipePic.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const RecipePage = () => {
-  const mockIngredients = [
-    "400 g of your favourite spaghetti",
-    <span className="green-leaf"><FontAwesomeIcon icon={faLeaf} /> 1 cup of fresh basil leaves</span>,
-    <span className="green-leaf"><FontAwesomeIcon icon={faLeaf} /> 4 garlic cloves</span>,
-    "1/4 cup olive oil",
-    "Salt and pepper to taste"
-  ];
-
-  const mockInstructions = [
-    "Cook 400 g pasta according to the instructions on the package.",
-    "While the pasta is cooking, chop about 6-8 tomatoes, use 1 cup fresh basil leaves and mince 4 garlic cloves.",
-    "In a pan, heat 1/4 cup of olive oil over medium heat, then add the chopped garlic and fry for a minute.",
-    "Add the chopped tomatoes and cook for a few minutes until soft.",
-    "Stir in the fresh basil and season with salt and pepper.",
-    "Mix the cooked pasta with the tomato basil sauce.",
-    "Serve and, if desired, sprinkle with grated Parmesan cheese."
-  ];
+const RecipePage = (props) => {
 
   return (
     <div className="recipe-page">
       {/* Image */}
-      <img src={PastaPesto} alt="Recipe" className="recipe-image" />
+      <img src={props.image} alt="Recipe" className="recipe-image" />
 
       {/* Recipe Title and Icon */}
       <div className="recipe-header">
-        <h1 className="recipe-title">Recipe Title</h1>
+        <h1 className="recipe-title">{props.title}</h1>
         <span className="heart-icon">
           <FontAwesomeIcon icon={faHeartRegular} />
         </span>
       </div>
 
-      {/* Icons with Text */}
+        {/* Icons with Text */}
       <div className="icon-list">
-        <div className="icon-item">
-          <FontAwesomeIcon icon={faClock} />
-          <div className="icon-text-container">
-            <p>30 min</p>
-          </div>
-        </div>
-        <div className="icon-item">
-          <FontAwesomeIcon icon={faUtensils} />
-          <div className="icon-text-container">
-            <p>Main Dish</p>
-          </div>
-        </div>
-        <div className="icon-item">
-          <FontAwesomeIcon icon={faUsers} />
-          <div className="icon-text-container">
-            <p>4 people</p>
-          </div>
+      <div className="icon-item">
+        <FontAwesomeIcon icon={faClock} />
+        <div className="icon-text-container">
+          <p>{props.cookingTime} min</p>
         </div>
       </div>
-
+      <div className="icon-item">
+        <FontAwesomeIcon icon={faUtensils} />
+        <div className="icon-text-container">
+          <p>{props.dishType}</p>
+        </div>
+      </div>
+      <div className="icon-item">
+        <FontAwesomeIcon icon={faUsers} />
+        <div className="icon-text-container">
+          <p>{props.servings} people</p>
+        </div>
+      </div>
+    </div>
+          
+      
       {/* Ingredients */}
       <div className="ingredients-list">
         <p className="section-title">Ingredients</p>
         <ul>
-          {mockIngredients.map((ingredient, index) => (
+          {props.ingredients.map((ingredient, index) => (
             <li key={index}>{ingredient}</li>
           ))}
         </ul>
@@ -72,7 +55,7 @@ const RecipePage = () => {
       <div className="instructions-list">
         <p className="section-title">Instructions</p>
         <ol>
-          {mockInstructions.map((instruction, index) => (
+          {props.instructions.map((instruction, index) => (
             <li key={index}>{instruction}</li>
           ))}
         </ol>
@@ -83,6 +66,7 @@ const RecipePage = () => {
         <button className="buy-button">Buy this recipe</button>
         <button className="suggest-button">Suggest Similar</button>
       </div>
+      
     </div>
   );
 }
