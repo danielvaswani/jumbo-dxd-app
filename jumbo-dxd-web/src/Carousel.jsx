f8cf3c159781435a10a3865b6ff1703242998d78
damiancaelen <475794@student.fontys.nl>
2023-12-04 13:41:54 +0100

    adjusted router linking

--- jumbo-dxd-web/src/Carousel.jsx
+++ jumbo-dxd-web/src/Carousel.jsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import Slider from 'react-slick';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 import './Carousel.css';
 import 'slick-carousel/slick/slick.css';
@@ -12,29 +11,26 @@ import bannerImage from './assets/banner1.png';
 
 const data = [
   {
-    id: 1,
     title: 'Cookie',
     text: 'Excluding fresh- and gingerbread',
     imageUrl: bannerImage,
   },
   {
-    id: 2,
     title: 'Cookie',
     text: 'Excluding fresh- and gingerbread',
     imageUrl: bannerImage,
   },
   {
-    id: 3,
     title: 'Cookie',
     text: 'Excluding fresh- and gingerbread',
     imageUrl: bannerImage,
   },
   {
-    id: 4,
     title: 'Cookie',
     text: 'Excluding fresh- and gingerbread',
     imageUrl: bannerImage,
   },
+  
 ];
 
 const settings = {
@@ -61,16 +57,14 @@ const HorizontalCarousel = () => {
       <h1 className="carousel-title">Get points easily</h1>
       <p className="carousel-description">Get points by buying reward products</p>
       <Slider className='slider' {...settings}>
-        {data.map((item) => (
-          <Link key={item.id} to={`/details/${item.id}`} className="card-link">
-            <div className="rounded-card">
-              <img className='imageCard' src={item.imageUrl} alt={item.title} />
-              <div className='cardContent'>
-                <h2>{item.title}</h2>
-                <p className='greydescription'>{item.text}</p>
-              </div>
+        {data.map((item, index) => (
+          <div key={index} className="rounded-card">
+            <img className='imageCard' src={item.imageUrl} alt={item.title} />
+            <div className='cardContent'>
+              <h2>{item.title}</h2>
+              <p className='greydescription'>{item.text}</p>
             </div>
-          </Link>
+          </div>
         ))}
       </Slider>
     </div>
