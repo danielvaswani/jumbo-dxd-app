78517796e475ab00a3296d5d1c34f29b78c2702a
Prakasa <i450220@fhict.nl>
2023-12-12 01:39:22 +0100

    generating description and refactored

--- jumbo-dxd-web/src/AiGeneratedRecipe.jsx
+++ jumbo-dxd-web/src/AiGeneratedRecipe.jsx
@@ -2,37 +2,67 @@ import React, { useRef, useState } from 'react';
 
 const AiGeneratedRecipe = () => {
   const [image_url, setImage_url] = useState('/');
-  let inputRef = useRef(null);
+  const [generatedDescription, setGeneratedDescription] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const inputRef = useRef(null);
 
-  const imageGenerator = async () => {
-    if (inputRef.current.value === '') {
-      return 0;
-    }
-    const response = await fetch(
-      'https://api.openai.com/v1/images/generations',
-      {
+  const generateContent = async () => {
+    try {
+      const imageResponse = await fetch(
+        'https://api.openai.com/v1/images/generations',
+        {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+            Authorization: 'Bearer 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',
+            'User-Agent': 'Chrome',
+          },
+          body: JSON.stringify({
+            prompt: `${inputValue}`,
+            n: 1,
+            model: 'dall-e-3',
+            size: '1024x1024',
+          }),
+        }
+      );
+      const imageData = await imageResponse.json();
+      const imageUrl = imageData.data[0].url;
+      setImage_url(imageUrl);
+
+      const descriptionResponse = await fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
         headers: {
-          'Content-type': 'application/json',
-        
-          Authorization:
-            'Bearer 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',
-          'User-Agent': 'Chrome',
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer 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',
         },
         body: JSON.stringify({
-            
-          prompt: `${inputRef.current.value}`,
-          n: 1,
-          model: 'dall-e-3',
-          size: "1024x1024"
-          ,
+          model: 'gpt-3.5-turbo',
+          messages: [
+            { role: 'system', content: `Give ingredients and description of ${inputValue} and keep it consistent and list the ingredients seperstely and description in steps below` },
+          ],
         }),
+      });
+
+      if (!descriptionResponse.ok) {
+        const errorData = await descriptionResponse.json();
+        console.error('Error Data:', errorData);
+        throw new Error('Network response was not ok.');
       }
-    );
-    let data = await response.json();
-    let data_array = data.data;
-    setImage_url(data_array[0].url);
-    console.log(data);
+
+      const descriptionData = await descriptionResponse.json();
+      setGeneratedDescription(descriptionData.choices[0].message.content);
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    generateContent();
   };
 
   return (
@@ -49,10 +79,16 @@ const AiGeneratedRecipe = () => {
           ref={inputRef}
           className='search-input'
           placeholder='Search for a recipe'
+          value={inputValue}
+          onChange={handleInputChange}
         />
-        <button className='generate-btn' onClick={() => imageGenerator()}>
+        <button className='generate-btn' onClick={handleFormSubmit}>
           Generate
         </button>
+      </div>
+      <div>
+        <h3>Generated Description:</h3>
+        <p>{generatedDescription}</p>
       </div>
     </div>
   );
