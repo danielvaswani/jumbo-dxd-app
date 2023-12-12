6ecf24cdd37d4dcbcc019f85dbf26f1d9723bf7e
Vaswani,Daniel D <d.vaswani@student.fontys.nl>
2023-12-12 03:44:52 +0100

    gpt4

--- jumbo-dxd-web/src/store/aiRecipeGenerator.js
+++ jumbo-dxd-web/src/store/aiRecipeGenerator.js
@@ -31,7 +31,7 @@ export const generateContent = async (inputValue) => {
               import.meta.env.VITE_OPENAI_API_KEY,
           },
           body: JSON.stringify({
-            model: 'gpt-3.5-turbo',
+            model: 'gpt-4',
             messages: [
               {
                 role: 'system',
