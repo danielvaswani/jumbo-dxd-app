0f553d4e03cf20bfb15c662022b596a25e03353c
Daniel Vaswani <vaswanidaniel@gmail.com>
2023-12-13 13:48:08 +0100

    finish sustainable

--- jumbo-dxd-web/src/components/CategorySection.jsx
+++ jumbo-dxd-web/src/components/CategorySection.jsx
@@ -1,36 +1,38 @@
-
 import React from "react";
 import "./CategorySection.css";
 
-
 const CategorySection = (props) => {
-    const category = props.category;
-    const alternateColor = props.alternateColor ?? false;
+  const category = props.category;
+  const alternateColor = props.alternateColor ?? false;
 
-    return (
-      <div className="category-section">
-         <h3>{category.title}</h3>
-         <p>{category.description}</p>
-         <div className="recipe-cards-container">
-         {category.recipeList.map(r => {
-            return (
-              
-                <div className="recipe-card" style={{ backgroundColor: alternateColor ? '#ffffff' : ' #2d8144', border: '1px solid #E2E2E2', borderStyle: alternateColor ? 'solid' : 'none'}}>
-                <img src={r.image} alt={r.title} />
-                <div className="recipe-extras">
-               <h3>{r.title}</h3>
-            
-               <p>{r.numberOfIngredients} Ingredients</p>
-               <p>{r.prepTime} Minutes</p>
-                </div>
-                </div>
-            )
-         })}
-          </div>
-      
+  return (
+    <div className="category-section flex flex-col gap-2">
+      <h3 className="font-bold">{category.title}</h3>
+      <p>{category.description}</p>
+      <div className="recipe-cards-container">
+        {category.recipeList.map((r) => {
+          return (
+            <div
+              className="recipe-card gap-3"
+              style={{
+                backgroundColor: alternateColor ? "#ffffff" : " #2d8144",
+                border: "1px solid #E2E2E2",
+                borderStyle: alternateColor ? "solid" : "none",
+              }}
+            >
+              <img src={r.image} alt={r.title} />
+              <div className="recipe-extras">
+                <h3 className="font-bold">{r.title}</h3>
+
+                <p className="text-xs">{r.numberOfIngredients} Ingredients</p>
+                <p className="text-xs">{r.prepTime} Minutes</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    );
-  }
-  
-  export default CategorySection;
-  
+    </div>
+  );
+};
+
+export default CategorySection;
